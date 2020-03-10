b00f0950dcfa70027efa79e5b48138daf404ef86
Jonathan Widro <jwidro@gmail.com>
2020-03-09 17:34:34 -0700

    add bootstrap, router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import { BrowserRouter } from "react-router-dom";
+
+import {Container, Row, Col} from 'react-bootstrap';
 
 import { Form } from './components/Form.js';
 import { NewsAPI } from './components/NewsAPI.js';
@@ -8,15 +13,13 @@ import { NewsAPI } from './components/NewsAPI.js';
 
 
 
-
 function App() {
   return (
-    <>
-    <Form />
-    <hr />
-    <NewsAPI />
-    </>
-
+        <Container>
+        	<Row>
+        		<h4>Company Lookup</h4>
+        	</Row>
+        </Container>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function Nav() {
+  return (
+    <nav>
+    	<h3>Logo</h3>
+    	<ul>
+    		<li>Form</li>
+    		<li>News</li>
+    	</ul>
+    </nav>
+
+  );
+}
+
+export default App;

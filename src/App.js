7a117c405c9b781394ecd0944f2f16dd285ffb71
Jonathan Widro <jwidro@gmail.com>
2020-03-09 20:18:51 -0700

    redux work

--- src/App.js
+++ src/App.js
@@ -13,19 +13,32 @@ import AlexaAPI from './components/AlexaAPI.js';
 
 import { createStore } from 'redux';
 
-const reducer =  (state, action) => {
+const initialState = {
+	result: 1, 
+	lastValues: []
+};
+
+const reducer = (state = initialState, action) => {
 	switch(action.type){
 		case "ADD":
-			state = state + action.payload;
+			state = {
+				...state,
+				result: state.result + action.payload, 
+				lastValues: [...state.lastValues, action.payload]
+			};
 			break;
 		case "SUBTRACT":
-			state = state - action.payload;
+			state = {
+				...state,
+				result: state.result - action.payload, 
+				lastValues: [...state.lastValues, action.payload]
+			};
 			break;
 	}
 	return state;
 };
 
-const store = createStore(reducer,1);
+const store = createStore(reducer);
 
 store.subscribe(() => {
 	console.log("Store updated!", store.getState());
